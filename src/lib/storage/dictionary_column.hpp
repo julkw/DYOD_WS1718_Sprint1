f663e904b650022ee6c9b8232d1ebc1dee04f17d
Juliane Waack <jkwaack@gmail.com>
2017-11-11 07:21:41 -0800

    add lower and upper bound

--- src/lib/storage/dictionary_column.hpp
+++ src/lib/storage/dictionary_column.hpp
@@ -110,17 +110,41 @@ class DictionaryColumn : public BaseColumn {
 
   // returns the first value ID that refers to a value >= the search value
   // returns INVALID_VALUE_ID if all values are smaller than the search value
-  ValueID lower_bound(T value) const;
+  ValueID lower_bound(T value) const
+  {
+      auto it = std::lower_bound(_dictionary->begin(), _dictionary->end(), value);
+      if (it == _dictionary->end()) {return INVALID_VALUE_ID;}
+      else {return ValueID(std::distance(_dictionary->begin(), it));}
+  }
 
   // same as lower_bound(T), but accepts an AllTypeVariant
-  ValueID lower_bound(const AllTypeVariant& value) const;
+  ValueID lower_bound(const AllTypeVariant& value) const
+  {
+      T val = dynamic_cast<T>(value);
+      if(!val){return INVALID_VALUE_ID;}
+      auto it = std::lower_bound(_dictionary->begin(), _dictionary->end(), val);
+      if (it == _dictionary->end()) {return INVALID_VALUE_ID;}
+      else {return ValueID(std::distance(_dictionary->begin(), it));}
+  }
 
   // returns the first value ID that refers to a value > the search value
   // returns INVALID_VALUE_ID if all values are smaller than or equal to the search value
-  ValueID upper_bound(T value) const;
+  ValueID upper_bound(T value) const
+  {
+      auto it = std::upper_bound(_dictionary->begin(), _dictionary->end(), value);
+      if (it == _dictionary->end()) {return INVALID_VALUE_ID;}
+      else {return ValueID(std::distance(_dictionary->begin(), it));}
+  }
 
   // same as upper_bound(T), but accepts an AllTypeVariant
-  ValueID upper_bound(const AllTypeVariant& value) const;
+  ValueID upper_bound(const AllTypeVariant& value) const
+  {
+      T val = dynamic_cast<T>(value);
+      if(!val){return INVALID_VALUE_ID;}
+      auto it = std::upper_bound(_dictionary->begin(), _dictionary->end(), val);
+      if (it == _dictionary->end()) {return INVALID_VALUE_ID;}
+      else {return ValueID(std::distance(_dictionary->begin(), it));}
+  }
 
   // return the number of unique_values (dictionary entries)
   size_t unique_values_count() const
